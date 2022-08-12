fab48b32413b864a9cfaeb294f726806670b9b78
Nicolas Audinet <nic.audinet27@gmail.com>
2022-08-12 11:48:30 +0200

    Start using Data.Grid

--- Main.hs
+++ Main.hs
@@ -1,26 +1,31 @@
+{-# LANGUAGE DataKinds #-}
+
 module Main where
 
+import Data.Grid
 import Data.List (intercalate)
+import Data.Maybe (fromJust)
 
 data TileType = Mine | Num Int | Empty
 data TileStatus = Hidden | Free
 data Tile = Tile TileStatus TileType
-newtype Grid = Grid { unGrid :: [[Tile]] }
 
-initGrid :: IO Grid
-initGrid = pure $ Grid
+type GridSize = '[3,3]
+type MineField = Grid GridSize Tile
+
+initGrid :: IO MineField
+initGrid = pure . fromJust . fromNestedLists $
   [[th1, th1, tf1]
   ,[th1, tm, tf1]
   ,[tf1, tf1, tf1]
   ]
-
   where
     th1 = Tile Hidden (Num 1)
     tf1 = Tile Free (Num 1)
     tm = Tile Hidden Mine
 
-showGrid :: Grid -> String
-showGrid = intercalate "\n" . map showRow . unGrid
+showGrid :: MineField -> String
+showGrid = intercalate "\n" . map showRow . toNestedLists
   where
     showRow :: [Tile] -> String
     showRow = mconcat . map showTile
@@ -34,7 +39,7 @@ showGrid = intercalate "\n" . map showRow . unGrid
     showTileType (Num i) = show i
     showTileType Empty = "*"
 
-printGrid :: Grid -> IO ()
+printGrid :: MineField -> IO ()
 printGrid = putStrLn . showGrid
 
 main :: IO ()
